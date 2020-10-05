4a6520415f17a8b3d2c09503b17e4aac4e37b240
Aditya Penmesta <adhi@meetyogi.com>
2020-10-05 17:45:53 -0400

    added price anaylsis and websocket

--- investing.py
+++ investing.py
@@ -1,20 +1,57 @@
 import requests
 
 key = "1AJHCKC421EAJDT9"
-response = requests.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=SQ&apikey=" + key)
+url = "https://www.alphavantage.co/query?"
+symbol = input("What symbol do you want? ")
+
+response = requests.get(url + "function=OVERVIEW&symbol=" + symbol + "&apikey=" + key)
 res = response.json()
+
+description = ""
+year_high = 0
+year_low = 0
+fifty_moving_avg = 0
+two_hundy_moving_avg = 0
 
 for key, value in res.items():
     if key == "Description":
-        print(value)
+        description = value
+    if key == "52WeekHigh":
+        year_high = value
+    if key == "52WeekLow":
+        year_low = value
+    if key == "50DayMovingAverage":
+        fifty_moving_avg = value
+    if key == "200DayMovingAverage":
+        two_hundy_moving_avg = value
 
-response1 = requests.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=" + key)
-res1 = response1.json()
+#print(description)
 
-for key, value in res1.items():
-    for ke, v in value.items():
-       print(v)
+print("52 Week High: {0}".format(year_high))
+print("52 Week Low: {0}".format(year_low))
+print("50 Day Moving Average: {0}".format(fifty_moving_avg))
+print("200 Day Moving Average: {0}".format(two_hundy_moving_avg))
 
-response2 = requests.get("https://www.alphavantage.co/query?function=SMA&symbol=IBM&interval=daily&time_period=10&series_type=open&apikey=" + key)
-res2 = response2.json()
+
+#Using finnhub.io, for NLP
+# List webhook
+r2 = requests.get('https://finnhub.io/api/v1/news-sentiment?symbol=TSLA&token=bttm65v48v6ojt2heeq0')
+res2 = r2.json()
 print(res2)
+
+
+
+#response1 = requests.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=" + key)
+#res1 = response1.json()
+
+#for key, value in res1.items():
+#    for ke, v in value.items():
+#       print(v)
+
+#response2 = requests.get("https://www.alphavantage.co/query?function=SMA&symbol=IBM&interval=daily&time_period=10&series_type=open&apikey=" + key)
+#res2 = response2.json()
+
+#for key, value in res2.items():
+#    for k, v in value.items():
+#        print(k)
+#print(res2)
