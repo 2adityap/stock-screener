8923515be8458e541373600c822b0277549ea603
Aditya Penmesta <adhi@meetyogi.com>
2020-12-11 16:41:14 -0500

    refactorization, enhanced querying

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -3,18 +3,21 @@ import nltk
 import os
 
 
+#check if there is a relationship between sentiment and stock price, then develop formula using gradient descent?
+
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 vader = SentimentIntensityAnalyzer()
 
 
-
+#Get keys from Twitter API
 CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
-print(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
+
+#authorize tweepy
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
@@ -26,26 +29,40 @@ try:
 except:
     print("Error during authentication")
 
-username = '_pshah'
-count = 20
+def pull_tweets(query, count):
+    tweet_texts = []
+    try:
+        tweets = tweepy.Cursor(api.search,q=query).items(count)
+        for tweet in tweets:
+            tweet_texts.append(tweet.text)
+    except:
+        pass
+    return tweet_texts
 
-tweet_texts = []
+def analyze_sentiments(tweets_list):
+    sentiment_list = []
+    for tweet in tweets_list:
+        sentiment_list.append(vader.polarity_scores(tweet)["compound"])
+    return sentiment_list
 
-try:
- # Creation of query method using parameters
- tweets = tweepy.Cursor(api.user_timeline,id=username).items(count)
- for tweet in tweets:
-    tweet_texts.append(tweet.text)
-except:
-    pass
+def most_negative_tweet(sentiment_list, tweets_list):
+    min_sentiment = min(sentiment_list)
+    index = sentiment_list.index(min_sentiment)
+    return tweets_list[index]
 
-sentiment_list = []
-for tweet in tweet_texts:
-    sentiment_list.append(vader.polarity_scores(tweet)["compound"])
+def most_positive_tweet(sentiment_list, tweets_list):
+    max_sentiment = max(sentiment_list)
+    index = sentiment_list.index(max_sentiment)
+    return tweets_list[index]
 
-max_sentiment = max(sentiment_list)
-max_index = sentiment_list.index(max_sentiment)
+def main():
+    query = '$SQ'
+    count = 100
+    tweets = pull_tweets(query,count)
+    sentiments = analyze_sentiments(tweets)
+    print(most_negative_tweet(sentiments, tweets))
+    print(most_positive_tweet(sentiments, tweets))
+    print((sum(sentiments))/(len(sentiments)))
 
-print(max_index)
-
-print(tweet_texts[max_index])
+if __name__ == "__main__":
+    main()
